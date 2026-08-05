8915a2ab07b019bafe7b106b708ea94c8efea5f3
Ajay Prakash <21376002@pondiuni.ac.in>
2026-08-05 23:56:35 +0530

    Writing Auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,38 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    // Route Handler
-    console.log("Handling the route user 1!!");
-    next();
-    // res.send("Response 1");
-  },
-  (req, res, next) => {
-    // Route Handler 2
-    console.log("Handling the route user 2!!");
-    // res.send("Response 2");
-    next();
-  },
-  (req, res, next) => {
-    // Route Handler 2
-    console.log("Handling the route user 3!!");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    // Route Handler 2
-    console.log("Handling the route user 4!!");
-    // res.send("Response 4");
-    next();
-  },
-  (req, res, next) => {
-    // Route Handler 2
-    console.log("Handling the route user 5!!");
-    res.send("Response 5");
-  },
-);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+// No need to check userAuth in user/login route handler
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully");
+});
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent!");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a User");
+});
 
 app.listen(port, (req, res) => {
   console.log(`Server is successfully listening on port ${port}`);
