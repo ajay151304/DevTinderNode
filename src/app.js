dbebcfd00049b21c2a70cdb39e45a0cc6a79ad9e
Ajay Prakash <21376002@pondiuni.ac.in>
2026-08-06 00:39:03 +0530

    Wild -Card Error Handling

--- src/app.js
+++ src/app.js
@@ -2,25 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-// No need to check userAuth in user/login route handler
-app.post("/user/login", (req, res) => {
-  res.send("User logged in successfully");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
-app.get("/user", userAuth, (req, res) => {
+app.get("/getUserData", (req, res) => {
+  // try {
+  // Logic of DB call and get user Data
+  throw new Error("dvdsvshhs");
   res.send("User Data Sent");
+  // } catch (err) {
+  // res.status(500).send("Some Error contact support team");
+  // }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent!");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a User");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(port, (req, res) => {
