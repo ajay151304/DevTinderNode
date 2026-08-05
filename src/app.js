e2d89bd2ccb71df45eb79f1892bce2c97f601b1e
Ajay Prakash <21376002@pondiuni.ac.in>
2026-08-05 14:01:51 +0530

    exploring GET POSt DELETE API

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/", (req, res) => {
-  res.send("Namaste from the dashboard");
+app.use("/user", (req, res) => {
+  res.send("wild card HAHAHAHAHAH");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ajay", lastName: "Prakash" });
+});
+app.post("/user", (req, res) => {
+  // Saving data to DB
+  res.send("Data successfully saved to the DB");
 });
 
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+// This will match all the HTTP method API call to /test
 app.use("/test", (req, res) => {
-  res.send("Hello from server");
+  res.send("Hello from the server");
 });
 
 app.listen(port, (req, res) => {
