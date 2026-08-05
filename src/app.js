71f3248c8de1722519533da97dcb30a839022841
Ajay Prakash <21376002@pondiuni.ac.in>
2026-08-05 16:10:28 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,11 +2,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// /ac, /abc
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Ajay", lastName: "Prakash" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // Route Handler
+    console.log("Handling the route user 1!!");
+    next();
+    // res.send("Response 1");
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 2!!");
+    // res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 3!!");
+    // res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 4!!");
+    // res.send("Response 4");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user 5!!");
+    res.send("Response 5");
+  },
+);
 
 app.listen(port, (req, res) => {
   console.log(`Server is successfully listening on port ${port}`);
