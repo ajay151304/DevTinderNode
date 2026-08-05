ae4b5177c6213b7ca0e25c52c03efbc124dbee6e
Ajay Prakash <21376002@pondiuni.ac.in>
2026-08-05 14:50:58 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/user", (req, res) => {
-  res.send("wild card HAHAHAHAHAH");
-});
-
-app.get("/user", (req, res) => {
+// /ac, /abc
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Ajay", lastName: "Prakash" });
-});
-app.post("/user", (req, res) => {
-  // Saving data to DB
-  res.send("Data successfully saved to the DB");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-// This will match all the HTTP method API call to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
 });
 
 app.listen(port, (req, res) => {
